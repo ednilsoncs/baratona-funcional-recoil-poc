668bcc157d1e0ee514394d4014f2812b76c750ac
ednilsoncs <ednilsonsantos43@gmail.com>
2021-06-30 11:33:13 -0300

    feat: adicionado estilização para o menu e para o header

--- src/screens/admin/AddProduct/index.js
+++ src/screens/admin/AddProduct/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Appbar} from 'react-native-paper';
 import {View} from 'react-native';
 
 const AddProduct = () => {
-  return <View />;
+  const _handleAddProduct = () => console.log('Searching');
+
+  return (
+    <View>
+      <Appbar.Header
+        style={{
+          backgroundColor: '#001529',
+        }}>
+        <Appbar.Content title="Gerenciamento" />
+        <Appbar.Action icon="plus" onPress={_handleAddProduct} />
+      </Appbar.Header>
+    </View>
+  );
 };
 
 export default AddProduct;
